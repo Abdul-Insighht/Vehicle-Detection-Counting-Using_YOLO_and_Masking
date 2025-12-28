7cfd3263e4e71dd58dbeaa11cc9b069556ac2992
Hafiz Abdul Rehman <185350638+Abdul-Insighht@users.noreply.github.com>
2025-12-29 00:42:34 +0500

    Add files via upload

---
+++ project/constant/__init__.py
@@ -0,0 +1,7 @@
+model_path = 'yolov8s.pt'
+data_path = 'project/data/4.mp4'
+mask_path = 'project/data/mask.png'
+skip = 1
+allowed_classes = [1, 2, 3, 5, 7, 9, 11]
+frame_count = 0
+line_y_red = 460
